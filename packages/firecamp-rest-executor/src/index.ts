0889e032704785f217448998ce8d46d694a31afc
Nishchit14 <dnishchit@gmail.com>
2022-09-20 14:50:28 +0530

    chore(rest-executor): executor class has been optimized

--- packages/firecamp-rest-executor/src/index.ts
+++ packages/firecamp-rest-executor/src/index.ts
@@ -1,180 +1,182 @@
-import { ERestBodyTypes, IRest, IRestResponse } from '@firecamp/types'
-import { _array, _object, _table } from '@firecamp/utils'
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
-import HTTPS from 'https'
-import QueryString from 'qs'
-import _url from '@firecamp/url'
-import { isNode } from 'browser-or-node'
-import parseBody from './helpers/body'
-import { IRestExecutor } from './types'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import HTTPS from 'https';
+import QueryString from 'qs';
+import { isNode } from 'browser-or-node';
+import { ERestBodyTypes, IRest, IRestResponse } from '@firecamp/types';
+import { _array, _object, _table } from '@firecamp/utils';
+import _url from '@firecamp/url';
 
-export * from './script-runner'
+import parseBody from './helpers/body';
+import { IRestExecutor } from './types';
+export * from './script-runner';
 
 export default class RestExecutor implements IRestExecutor {
+  private _controller: AbortController;
 
-    controller: AbortController
+  constructor() {
+    this._controller = new AbortController();
+  }
 
-    constructor() {
-        this.controller = new AbortController()
+  private _timeline(
+    request: AxiosRequestConfig,
+    response: AxiosResponse
+  ): string {
+    const tl: string[] = [];
+
+    /**
+     * Return the key:value string
+     * @param object
+     * @param prefix
+     * @returns
+     */
+    const objectToText = (object = {}, prefix: string) =>
+      Object.keys(object).reduce(
+        (prev, key) => `${prev + prefix + ' ' + key}:${object[key]}\n`,
+        ''
+      );
+
+    const { status, statusText, config, headers } = response;
+
+    tl.push('\n----------------General----------------\n');
+    tl.push(`# Request URL:  ${config.url}`);
+    tl.push(`# Request Method: ${config.method}`);
+    tl.push(`# Status Code: ${status} ${statusText}`);
+
+    if (!_object.isEmpty(request.headers || {})) {
+      tl.push('\n-----------Request Headers-----------\n');
+      tl.push(objectToText(request.headers, '>'));
     }
 
-    async prepare(request: IRest): Promise<AxiosRequestConfig> {
-        const { meta, body, config, headers, url } = request
-
-        const axiosRequest: AxiosRequestConfig = {
-            url: _url.parse(url?.raw || '', ['http', 'https']),
-            params: QueryString.stringify(_table.toObject(url?.query_params || [])),
-            method: request.method,
-            headers: _table.toObject(headers || []),
-            // TODO: Supported in browser
-            httpsAgent: new HTTPS.Agent({ rejectUnauthorized: false }),
-            signal: this.controller.signal,
-            timeout: config?.request_timeout,
-            maxRedirects: config?.max_redirects,
-            transformResponse: (response) => response,
-        }
-
-        // disable SSL validation default
-        if (isNode) {
-            axiosRequest.httpsAgent = new HTTPS.Agent({
-                rejectUnauthorized: config?.reject_unauthorized
-            })
-        }
-
-        // parse path params
-        if (!_array.isEmpty(url?.path_params as any))
-            axiosRequest.url = _url.replacePathParams(url?.raw || '', url?.path_params || [])
-
-        // TODO: Check sending file without serialize in desktop environment
-        // parse body payload
-        axiosRequest.data = await parseBody(body || {}, meta?.active_body_type || ERestBodyTypes.NoBody)
-
-        return axiosRequest
+    if (typeof config.data === 'string') {
+      tl.push('\n-----------Request Data-----------\n');
+      tl.push(`> ${config.data}`);
     }
 
-    timeline(request: AxiosRequestConfig, response: AxiosResponse): string {
-        const timeline: string[] = []
+    tl.push(`\n-----------Response Headers-----------\n`);
+    tl.push(objectToText(headers, '<'));
 
-        /**
-         * Return the key:value string
-         * @param object 
-         * @param prefix 
-         * @returns 
-         */
-        const objectToText = (object = {}, prefix: string) =>
-            Object.keys(object).reduce(
-                (prev, key) => `${prev + prefix + ' ' + key}:${object[key]}\n`,
-                ''
-            )
-
-        const { status, statusText, config, headers } = response
-
-        timeline.push('\n----------------General----------------\n')
-
-        timeline.push(`# Request URL:  ${config.url}`)
-
-        timeline.push(`# Request Method: ${config.method}`)
-
-        timeline.push(`# Status Code: ${status} ${statusText}`)
-
-        if (!_object.isEmpty(request.headers || {})) {
-
-            timeline.push('\n-----------Request Headers-----------\n')
-
-            timeline.push(objectToText(request.headers, '>'))
-        }
-
-        if (typeof config.data === 'string') {
-            timeline.push('\n-----------Request Data-----------\n')
-
-            timeline.push(`> ${config.data}`)
-        }
-
-        timeline.push(`\n-----------Response Headers-----------\n`)
-
-        timeline.push(objectToText(headers, '<'))
-
-        if (typeof response.data === 'string') {
-            timeline.push('\n-----------Response Data-----------\n')
-
-            timeline.push(`> ${response.data}`)
-        }
-
-        return timeline.join('\n')
+    if (typeof response.data === 'string') {
+      tl.push('\n-----------Response Data-----------\n');
+      tl.push(`> ${response.data}`);
     }
 
-    normalizeResponse(axiosResponse: AxiosResponse): IRestResponse {
-        return {
-            statusCode: axiosResponse.status,
-            statusMessage: axiosResponse.statusText,
-            data: axiosResponse.data,
-            headers: axiosResponse.headers,
-            duration: axiosResponse?.config?.['metadata']?.duration || 0,
-            size: Number(axiosResponse?.headers?.['content-length']) || 0
+    return tl.join('\n');
+  }
+
+  private _normalizeResponse(axiosResponse: AxiosResponse): IRestResponse {
+    return {
+      statusCode: axiosResponse.status,
+      statusMessage: axiosResponse.statusText,
+      data: axiosResponse.data,
+      headers: axiosResponse.headers,
+      //@ts-ignore
+      duration: axiosResponse?.config?.metadata?.duration || 0,
+      size: Number(axiosResponse?.headers?.['content-length']) || 0,
+    };
+  }
+
+  /**
+   * Return axios request config generated from Firecamp REST request
+   * @param request: IRest
+   */
+  private async _prepare(request: IRest): Promise<AxiosRequestConfig> {
+    const { meta, body, config, headers, url } = request;
+
+    const axiosRequest: AxiosRequestConfig = {
+      url: _url.parse(url?.raw || '', ['http', 'https']),
+      params: QueryString.stringify(_table.toObject(url?.query_params || [])),
+      method: request.method,
+      headers: _table.toObject(headers || []),
+      // TODO: Supported in browser
+      httpsAgent: new HTTPS.Agent({ rejectUnauthorized: false }),
+      signal: this._controller.signal,
+      timeout: config?.request_timeout,
+      maxRedirects: config?.max_redirects,
+      transformResponse: (response) => response,
+    };
+
+    // disable SSL validation default
+    if (isNode) {
+      axiosRequest.httpsAgent = new HTTPS.Agent({
+        rejectUnauthorized: config?.reject_unauthorized,
+      });
+    }
+
+    // parse path params
+    if (!_array.isEmpty(url?.path_params as any))
+      axiosRequest.url = _url.replacePathParams(
+        url?.raw || '',
+        url?.path_params || []
+      );
+
+    // TODO: Check sending file without serialize in desktop environment
+    // parse body payload
+    axiosRequest.data = await parseBody(
+      body || {},
+      meta?.active_body_type || ERestBodyTypes.NoBody
+    );
+
+    return axiosRequest;
+  }
+
+  async send(request: IRest): Promise<IRestResponse> {
+    let axiosRequest: AxiosRequestConfig = {};
+
+    try {
+      if (_object.isEmpty(request))
+        return Promise.reject(new Error('Invalid request payload'));
+
+      axiosRequest = await this._prepare(request);
+
+      // note the request start time
+      axios.interceptors.request.use((request) => {
+        request['metadata'] = {
+          startTime: new Date(),
+        };
+        return request;
+      });
+
+      // note the request finished time
+      axios.interceptors.response.use((res) => {
+        res.config['metadata']['endTime'] = new Date();
+        res.config['metadata'] = {
+          ...res.config['metadata'],
+          get duration() {
+            return this.endTime - this.startTime;
+          },
+        };
+        return res;
+      });
+
+      // execute request
+      const axiosResponse = await axios(axiosRequest);
+
+      // normalize response according to Firecamp REST request's response
+      const response = this._normalizeResponse(axiosResponse);
+
+      // prepare timeline of request execution
+      response['timeline'] = this._timeline(axiosRequest, axiosResponse);
+
+      return Promise.resolve(response);
+    } catch (error) {
+      console.error(error);
+      if (!_object.isEmpty(error.response)) {
+        const response = this._normalizeResponse(error.response);
+
+        if (!error?.response?.config && error?.config) {
+          error.response.config = error.config;
         }
+
+        // prepare timeline of request execution
+        response['timeline'] = this._timeline(axiosRequest, error.response);
+
+        return Promise.reject(response);
+      }
+      return Promise.reject(error.message);
     }
+  }
 
-    async send(request: IRest): Promise<IRestResponse> {
-        let axiosRequest: AxiosRequestConfig = {}
-
-        try {
-            if (_object.isEmpty(request))
-                return Promise.reject(new Error('Invalid request payload'))
-
-            axiosRequest = await this.prepare(request)
-
-            // note the request start time
-            axios.interceptors.request.use((request) => {
-                request['metadata'] = {
-                    startTime: new Date()
-                }
-
-                return request
-            })
-
-            // note the request finished time
-            axios.interceptors.response.use((response) => {
-                response.config['metadata']['endTime'] = new Date()
-
-                response.config['metadata'] = {
-                    ...response.config['metadata'],
-                    get duration() {
-                        return this.endTime - this.startTime
-                    }
-                }
-
-                return response
-            })
-
-            // execute request
-            const axiosResponse = await axios(axiosRequest)
-
-            // normalize response according to Firecamp REST request's response
-            const response = this.normalizeResponse(axiosResponse)
-
-            // prepare timeline of request execution
-            response['timeline'] = this.timeline(axiosRequest, axiosResponse)
-
-            return Promise.resolve(response)
-        } catch (error) {console.error(error);
-        
-            if (!_object.isEmpty(error.response)) {
-                const response = this.normalizeResponse(error.response)
-
-                if (!error?.response?.config && error?.config) {
-                    error.response.config = error.config
-                }
-
-                // prepare timeline of request execution
-                response['timeline'] = this.timeline(axiosRequest, error.response)
-
-                return Promise.reject(response)
-            }
-            return Promise.reject(error.message)
-        }
-    }
-
-    cancel(): void {
-        this.controller.abort()
-    }
+  cancel() {
+    this._controller.abort();
+  }
 }
--- packages/firecamp-rest-executor/src/types.ts
+++ packages/firecamp-rest-executor/src/types.ts
@@ -1,28 +1,14 @@
-import { IRest, IRestResponse } from '@firecamp/types'
-import { AxiosRequestConfig } from 'axios'
+import { IRest, IRestResponse } from '@firecamp/types';
+import { AxiosRequestConfig } from 'axios';
 
 export interface IRestExecutor {
+  /**
+   * send the request to the server and return the response
+   * received from the server
+   * @param request REST request want to send to the server
+   */
+  send(request: IRest): Promise<IRestResponse>;
 
-    /**
-     * controller to abort the axios request execution
-     */
-    controller: AbortController
-
-    /**
-     * Return axios request config generated from Firecamp REST request
-     * @param request Firecamp REST request
-     */
-    prepare(request: IRest): Promise<AxiosRequestConfig>
-
-    /**
-     * send the request to the server and return the response
-     * received from the server
-     * @param request REST request want to send to the server
-     */
-    send(request: IRest): Promise<IRestResponse>
-
-    /**
-     * cancel the request
-     */
-    cancel(): void
+  /**  cancel the request */
+  cancel(): void;
 }
