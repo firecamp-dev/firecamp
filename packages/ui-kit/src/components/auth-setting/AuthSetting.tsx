d87dc503f644713341a39edf04b338554cf95fd5
Nishchit14 <dnishchit@gmail.com>
2022-09-21 09:45:16 +0000

    refactoring(auth): auth code optimization happned

--- packages/ui-kit/src/components/auth-setting/AuthSetting.tsx
+++ packages/ui-kit/src/components/auth-setting/AuthSetting.tsx
@@ -298,7 +298,7 @@ const AuthSetting: FC<IAuthSetting> = ({
             <Notes
               type="info"
               title="Coming soon!!"
-              description={`Firecamp Team is building this feature and it’ll be releasing very soon. Keep your eyes open at <span>  <a href="https://github.com/firecamp-io/firecamp/releases" target="_blank">
+              description={`Firecamp team is building this feature and it’ll be releasing very soon. Keep us watching on <span>  <a href="https://github.com/firecamp-io/firecamp/releases" target="_blank">
 Github </a>, <a href="https://twitter.com/FirecampHQ" target="_blank">Twitter</a>, <a href="https://discord.com/invite/8hRaqhK" target="_blank"> Discord</a> </span>.`}
             />
           </div>
