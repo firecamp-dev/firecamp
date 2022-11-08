558250a9320cde41ace90216834709b04bfbe4ca
Nishchit14 <dnishchit@gmail.com>
2022-11-08 14:52:35 +0530

    chore: small fixes

--- packages/ui-kit/src/components/table-v3/MultipartTable.tsx
+++ packages/ui-kit/src/components/table-v3/MultipartTable.tsx
@@ -1,6 +1,6 @@
 import { _array } from '@firecamp/utils';
 import { GrDrag } from '@react-icons/all-files/gr/GrDrag';
-import { TiSortAlphabetically } from '@react-icons/all-files/ti/TiSortAlphabetically';
+import { VscTextSize } from '@react-icons/all-files/vsc/VscTextSize';
 import { VscAdd } from '@react-icons/all-files/vsc/VscAdd';
 import { VscFile } from '@react-icons/all-files/vsc/VscFile';
 import { VscTrash } from '@react-icons/all-files/vsc/VscTrash';
@@ -10,9 +10,7 @@ import Checkbox from '../checkbox/Checkbox';
 import SingleLineEditor from '../editors/monaco-v2/SingleLineEditor';
 import Table, { TTableApi } from './primitive/Table';
 
-import {
-  IMultiPartInput,
-} from './MultipartTable.interfaces';
+import { IMultiPartInput } from './MultipartTable.interfaces';
 
 const MultipartTable = ({
   name = '',
@@ -280,16 +278,16 @@ const MultiPartInput: FC<IMultiPartInput> = memo(
 
             <div
               key={`${row.id}-file-type`}
-              className="text-center text-sm text-base text-ellipsis overflow-hidden px-4 whitespace-pre"
+              className="text-center text-sm text-base text-ellipsis overflow-hidden pl-1 pr-4 whitespace-pre"
               onClick={_onClick}
             >
-              {row?.file?.name || 'select file'}
+              {row?.file?.name ? `file: ${row?.file?.name}` : 'select file'}
             </div>
           </>
         )}
-        <div className="absolute" style={{ right: '5px' }}>
+        <div className="absolute cursor-pointer" style={{ right: '5px' }}>
           {type == 'text' ? (
-            <TiSortAlphabetically onClick={_changeType} />
+            <VscTextSize onClick={_changeType} />
           ) : (
             <VscFile onClick={_changeType} />
           )}
