8306548a8b2830d3f385ed949156fd7c2dec8706
Nishchit14 <dnishchit@gmail.com>
2022-12-04 22:34:35 +0530

    feat: push action service deprecated and removed '

--- packages/firecamp-rest/src/store/pushAction.slice.ts
+++ packages/firecamp-rest/src/store/pushAction.slice.ts
@@ -1,7 +1,6 @@
 import _cleanDeep from 'clean-deep';
 import _cloneDeep from 'lodash/cloneDeep';
 import {
-  IUrl,
   EAuthTypes,
   ERequestTypes,
   TId,
@@ -9,8 +8,6 @@ import {
   IRest,
 } from '@firecamp/types';
 import { _array, _object } from '@firecamp/utils';
-import PushActionService from '../services/push-actions';
-import { TAuth } from './index';
 
 import {
   EReqChangeRootKeys,
@@ -67,81 +64,13 @@ interface IPushPayload extends Partial<IRest> {
 
 interface IPushActionSlice {
   pushAction?: IPushAction;
-  prepareAuthPushAction?: (
-    lastAuth: TAuth,
-    auth: TAuth,
-    authType: EAuthTypes
-  ) => void;
-  prepareActiveBodyTypePushAction?: (lastBodyType, bodyType) => void;
-  prepareBodyPushAction?: (lastBody, body, bodyType: string) => void;
   prepareRequestInsertPushPayload: () => Promise<IPushPayload>;
   prepareRequestUpdatePushPayload: () => Promise<IPushPayload>;
-  prepareRequestUpdatePushAction: (
-    request: Partial<IRestClientRequest>
-  ) => Promise<Partial<IPushAction>>;
   setPushActionEmpty: () => void;
 }
 
 const createPushActionSlice = (set, get): IPushActionSlice => ({
   pushAction: _cloneDeep(emptyPushAction),
-
-  prepareAuthPushAction: (
-    lastAuth: TAuth,
-    auth: TAuth,
-    authType: EAuthTypes
-  ) => {
-    // Return if request is not saved
-    if (!get()?.runtime?.isRequestSaved) return;
-
-    let authPushAction = PushActionService.prepareAuthPushAction(
-      lastAuth,
-      auth,
-      authType,
-      {
-        auth: get().pushAction?.auth || [],
-        _removed: get().pushAction?._removed,
-      }
-    );
-
-    set((s) => ({
-      ...s,
-      pushAction: {
-        ...s.pushAction,
-        auth: authPushAction.auth,
-        _removed: {
-          ...s.pushAction._removed,
-          auth: authPushAction._removed.auth,
-        },
-      },
-    }));
-  },
-
-  prepareBodyPushAction: (lastBody, body, bodyType: ERestBodyTypes) => {
-    // Return if request is not saved
-    if (!get()?.runtime?.isRequestSaved) return;
-
-    let bodyPushAction = PushActionService.prepareBodyPushAction(
-      lastBody,
-      body,
-      bodyType,
-      {
-        body: get().pushAction?.body || [],
-        _removed: get().pushAction?._removed,
-      }
-    );
-
-    set((s) => ({
-      ...s,
-      pushAction: {
-        ...s.pushAction,
-        body: bodyPushAction.body,
-        _removed: {
-          ...s.pushAction._removed,
-          body: bodyPushAction._removed.body,
-        },
-      },
-    }));
-  },
 
   prepareRequestInsertPushPayload: async (): Promise<IPushPayload> => {
     let request: IRestClientRequest = get().request;
@@ -218,62 +147,6 @@ const createPushActionSlice = (set, get): IPushActionSlice => ({
     return Promise.resolve(pushPayload);
   },
 
-  prepareRequestUpdatePushAction: (
-    request: Partial<IRestClientRequest>
-  ): Promise<Partial<IPushAction>> => {
-    let pushAction: Partial<IPushAction> = {};
-    let lastRequest: IRestClientRequest = get()?.last.request;
-
-    // console.log('prepareRequestUpdatePushAction: ', { lastRequest, request });
-
-    for (let key in request) {
-      switch (key) {
-        // handle auth
-        case 'auth':
-          for (let authType in request.auth) {
-            let authPushAction = PushActionService.prepareAuthPushAction(
-              lastRequest.auth[authType],
-              request.auth[authType],
-              authType as EAuthTypes
-              /* {
-                auth: get().pushAction?.auth || [],
-                _removed: get().pushAction?._removed,
-              } */
-            );
-
-            pushAction = _object.mergeDeep(
-              _cloneDeep(pushAction),
-              authPushAction
-            );
-          }
-          break;
-
-          break;
-
-        // handle body
-        case 'body':
-          for (let bodyType in request.body) {
-            let bodyPushAction = PushActionService.prepareBodyPushAction(
-              lastRequest.body[bodyType],
-              request.body[bodyType],
-              bodyType as ERestBodyTypes
-              /* {
-                body: get().pushAction?.body || [],
-                _removed: get().pushAction?._removed,
-              } */
-            );
-            pushAction = _object.mergeDeep(
-              _cloneDeep(pushAction),
-              bodyPushAction
-            );
-          }
-          break;
-      }
-    }
-    console.log({ prepareRequestUpdatePushAction: pushAction });
-    return Promise.resolve(pushAction);
-  },
-
   setPushActionEmpty: () => {
     // console.log({ emptyPushAction });
 
@@ -284,10 +157,4 @@ const createPushActionSlice = (set, get): IPushActionSlice => ({
   },
 });
 
-export {
-  createPushActionSlice,
-  IPushActionSlice,
-  IPushAction,
-  IPushPayload,
-  emptyPushAction,
-};
+export { createPushActionSlice, IPushActionSlice, IPushAction, IPushPayload };
