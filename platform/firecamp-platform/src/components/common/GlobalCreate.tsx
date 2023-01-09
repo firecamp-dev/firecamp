c5cedacc7c029f12725cf83810880da5acc8ef26
Nishchit14 <dnishchit@gmail.com>
2023-01-09 11:31:15 +0000

    feat: GlobalCreate menus function implemented

--- platform/firecamp-platform/src/components/common/GlobalCreate.tsx
+++ platform/firecamp-platform/src/components/common/GlobalCreate.tsx
@@ -1,28 +1,64 @@
 import { useState } from 'react';
 import classnames from 'classnames';
 import { Dropdown, Button } from '@firecamp/ui-kit';
+import platformContext from '../../services/platform-context';
+import { platformEmitter } from '../../services/platform-emitter';
+import { EPlatformTabs } from '../../services/platform-emitter/events';
+
+enum EMenuOptions {
+  Request = 'request',
+  Collection = 'collection',
+  Environment = 'environment',
+  ImportCollection = 'import-collection',
+  Workspace = 'workspace',
+  Organization = 'organization',
+  InviteMembers = 'invite-members',
+}
+const options = [
+  {
+    header: 'CREATE NEW',
+    list: [
+      { id: EMenuOptions.Request, name: 'REQUEST' },
+      { id: EMenuOptions.Collection, name: 'COLLECTION' },
+      { id: EMenuOptions.Environment, name: 'ENVIRONMENT' },
+      { id: EMenuOptions.ImportCollection, name: 'IMPORT COLLECTION' },
+    ],
+  },
+  {
+    header: 'Create New (by admin)',
+    list: [
+      { id: EMenuOptions.Workspace, name: 'WORKSPACE' },
+      { id: EMenuOptions.Organization, name: 'ORGANIZATION' },
+      { id: EMenuOptions.InviteMembers, name: 'INVITE MEMBERS' },
+    ],
+  },
+];
 
 const GlobalCreateDD = ({}) => {
   const [isOpen, toggleOpen] = useState(false);
-  const options = [
-    {
-      header: 'CREATE NEW',
-      list: [
-        { id: 'request', name: 'REQUEST' },
-        { id: 'collection', name: 'COLLECTION' },
-        { id: 'environment', name: 'ENVIRONMENT' },
-        { id: 'import-collection', name: 'IMPORT COLLECTION' },
-      ],
-    },
-    {
-      header: 'Create New (by admin)',
-      list: [
-        { id: 'request', name: 'WORKSPACE' },
-        { id: 'organization', name: 'ORGANIZATION' },
-        { id: 'invite-members', name: 'INVITE MEMBERS' },
-      ],
-    },
-  ];
+  const onSelect = (option) => {
+    switch (option.id) {
+      case EMenuOptions.Request:
+        platformEmitter.emit(EPlatformTabs.openNew, 'rest');
+        break;
+      case EMenuOptions.Collection:
+        break;
+      case EMenuOptions.Environment:
+        platformContext.app.modals.openCreateEnvironment();
+        break;
+      case EMenuOptions.ImportCollection:
+        break;
+      case EMenuOptions.Workspace:
+        platformContext.app.modals.openCreateWorkspace();
+        break;
+      case EMenuOptions.Organization:
+        platformContext.app.modals.openCreateOrg();
+        break;
+      case EMenuOptions.InviteMembers:
+        platformContext.app.modals.openInviteMembers();
+        break;
+    }
+  };
 
   return (
     <div className="border-l border-b border-tabBorder flex items-center pl-1">
@@ -46,7 +82,7 @@ const GlobalCreateDD = ({}) => {
         </Dropdown.Handler>
         <Dropdown.Options
           options={options}
-          onSelect={console.log}
+          onSelect={onSelect}
           headerMeta={{ applyUpperCase: true }}
           hasDivider={true}
           className="type-1"
