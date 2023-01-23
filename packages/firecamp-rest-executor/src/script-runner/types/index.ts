1a39e6bff23620fb84bb5774bc18f22e42601cca
Nishchit14 <dnishchit@gmail.com>
2023-01-23 13:34:48 +0000

    refactor(scripts): variables entity in script runner is being refactored, wip

--- packages/firecamp-rest-executor/src/script-runner/types/index.ts
+++ packages/firecamp-rest-executor/src/script-runner/types/index.ts
@@ -7,10 +7,26 @@ export * from './request'
 export * from './response'
 
 export type TEnvVariable = { [key: string]: number | boolean | string }
-export type TPreScript = (request: IRest, variables: TEnvVariable) => Promise<{ request: IScriptRequest, environment: IEnvironment }>
-export type TPostScript = (postScript: string, response: IRestResponse, variables: TEnvVariable) => Promise<{ response: IRestResponse, environment: IEnvironment }>
+export type TPreScript = (request: IRest, variables: TVariable) => Promise<{ request: IScriptRequest, environment: IEnvironment }>
+export type TPostScript = (postScript: string, response: IRestResponse, variables: TVariable) => Promise<{ response: IRestResponse, environment: IEnvironment }>
 export type TTestScript = (
     request: IRest,
     response: IRestResponse,
-    variables: TEnvVariable
+    variables: TVariable
 ) => Promise<any>
+
+
+export enum EVariableType {
+    text= 'text',
+    number= 'number',
+    boolean = 'boolean',
+    secret = 'secret'
+}
+
+export type TVariable = {
+    id?: string,
+    key: string,
+    value: string,
+    type?: EVariableType,
+    disable?: boolean
+}
---
+++ packages/firecamp-rest-executor/src/script-runner/variables.ts
@@ -0,0 +1,76 @@
+import { _object } from '@firecamp/utils';
+import { nanoid } from 'nanoid';
+import { EVariableType, TEnvVariable, TVariable } from './types';
+
+export class Variables implements IVariables {
+  private variables: TVariable[];
+  private unsetVariables: TVariable[] = [];
+
+  constructor(variables: TVariable[]) {
+    this.variables = variables;
+  }
+
+  has(variableName: string | number): boolean {
+    return !!this.variables.find((v) => v.key == variableName);
+  }
+
+  get(variableName: string | number) {
+    const variable = this.variables.find((v) => v.key == variableName);
+    if (!variable) return;
+    if (variable.type == 'text') return variable.value;
+    else if (variable.type == 'number') return +variable.value;
+    else if (variable.type == 'boolean') return !!variable.value;
+    else return variable.value;
+  }
+
+  set(variableName: string, variableValue: string | number | boolean) {
+    const variable = this.variables.find((v) => v.key == variableName);
+    const varType: EVariableType | string = typeof variableValue;
+    if (!['string', 'number', 'boolean'].includes(varType)) return;
+    //@ts-ignore
+    const type: EVariableType =
+      varType == 'string' ? EVariableType.text : varType;
+
+    if (variable) {
+      this.variables.map((v) => {
+        if (v.key == variableName) {
+          v.value = '' + variableValue;
+          v.type = type;
+        }
+        return v;
+      });
+    } else {
+      this.variables = [
+        ...this.variables,
+        { id: nanoid(), key: variableName, value: '' + variableValue, type },
+      ];
+    }
+  }
+
+  unset(variableName: string) {
+    const variable = this.variables.find((v) => v.key == variableName);
+    this.variables = this.variables.filter((v) => {
+      return v.key != variableName;
+    });
+    if (variable) this.unsetVariables.push(variable);
+  }
+
+  clear() {
+    this.variables = [];
+    this.unsetVariables = [];
+  }
+}
+
+export interface IVariables {
+  /**  variable exists */
+  has: (variableName: string | number) => boolean;
+
+  /** return the variable value */
+  get: (variableName: string | number) => string | number | boolean | undefined;
+
+  /** set variable name and value */
+  set: (variableName: string, variableValue: string | number | boolean) => void;
+
+  /** unset variable */
+  unset: (variableName: string) => void;
+}
