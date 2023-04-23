7a961b5bc0e6841d7f1ef2ff585aec2a4818a81a
Nishchit14 <dnishchit@gmail.com>
2023-04-23 08:22:40 +0000

    fix: ws collection multiple entry of folder issue has been fixed and collection maddItem method improved

--- packages/firecamp-websocket/src/components/sidebar-panel/tabs/CollectionTab.tsx
+++ packages/firecamp-websocket/src/components/sidebar-panel/tabs/CollectionTab.tsx
@@ -4,37 +4,11 @@ import { VscNewFolder } from '@react-icons/all-files/vsc/VscNewFolder';
 import { VscRefresh } from '@react-icons/all-files/vsc/VscRefresh';
 import { Tree, UncontrolledTreeEnvironment } from '@firecamp/ui/src/tree';
 import { Pane, ToolBar, Empty } from '@firecamp/ui';
-import { TId } from '@firecamp/types';
 import treeRenderer from './collection-tree/treeItemRenderer';
 import { useStore, useStoreApi, IStore } from '../../../store';
 
 const CollectionTab = () => {
-  const {
-    context,
-    isRequestSaved,
-    prepareCreateFolderPayload,
-    onCreateFolder,
-  } = useStore(
-    (s: IStore) => ({
-      isRequestSaved: s.runtime.isRequestSaved,
-      context: s.context,
-      prepareCreateFolderPayload: s.prepareCreateFolderPayload,
-      onCreateFolder: s.onCreateFolder,
-    }),
-    shallow
-  );
-
-  const _createFolderPrompt = async (parentFolderId?: TId) => {
-    if (typeof parentFolderId != 'string') parentFolderId = undefined;
-    if (!isRequestSaved) {
-      return context.app.notify.info(
-        'Please save the websocket request first.'
-      );
-    }
-    const _folder = prepareCreateFolderPayload('', parentFolderId);
-    context.request.createRequestFolderPrompt(_folder).then(onCreateFolder);
-  };
-
+  const { promptCreateFolder } = useStoreApi().getState() as IStore;
   return (
     <Pane
       expanded={true}
@@ -52,14 +26,14 @@ const CollectionTab = () => {
               <VscNewFolder
                 size={14}
                 className="cursor-pointer"
-                onClick={() => _createFolderPrompt()}
+                onClick={() => promptCreateFolder()}
               />
             </div>
           </ToolBar>
         );
       }}
       bodyRenderer={({ expanded }) => {
-        return <Collection openCreateFolderPrompt={_createFolderPrompt} />;
+        return <Collection openCreateFolderPrompt={promptCreateFolder} />;
       }}
     />
   );
@@ -163,7 +137,7 @@ const Collection = ({ openCreateFolderPrompt }) => {
         }
       >
         <Tree
-          treeId="fc-environment-tree"
+          treeId="fc-ws-message-collection-tree"
           rootItem="root"
           treeLabel="WebSocket Message Collection"
           ref={treeRef}
