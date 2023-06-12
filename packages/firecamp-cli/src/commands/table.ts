258608806d5fb10b47925f8cce3a63992eb9ef54
Nishchit Dhanani <dnishchit@gmail.com>
2023-06-12 18:55:03 +0530

    feat: table mock example added

---
+++ packages/firecamp-cli/src/commands/table.ts
@@ -0,0 +1,39 @@
+import { Command, ux } from '@oclif/core'
+import * as chalk from 'chalk';
+import * as Table from 'cli-table3';
+
+export default class Users extends Command {
+    static flags = {
+
+    }
+
+    async run() {
+        const { flags } = await this.parse(Users)
+        // const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users')
+
+        var table = new Table({
+            head: ['method', 'api', 'status', 'pass', 'fail'],
+            // colWidths: [100, 400, 200, 150, 150 ],
+            wordWrap: true,
+            chars: {
+                'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+                , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+                , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+                , 'right': '║', 'right-mid': '╢', 'middle': '│'
+            }
+        });
+
+        table.push(
+            [chalk.green('GET'), chalk.blue('https://localhost:300/api/post'), '200', 3, 2],
+            [chalk.blue('POST'), chalk.blue('https://localhost:300/api/post'), '200', 3, 1],
+            [chalk.red('DELETE'), chalk.blue('https://localhost:300/api/post'), '200', 3, 2]
+        );
+
+        console.log(table.toString());
+
+        // console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+        // boxen('unicorn', {padding: 1})
+
+    }
+}
