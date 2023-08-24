99865becbed85b1f890015b5ad2ca528056f9159
fraybabak <keanbabak@gmail.com>
2023-08-24 13:43:47 +0300

    feat: twitter badge and github star badge (#52)

--- platform/firecamp-platform/src/components/status-bar/StatusBarContainer.tsx
+++ platform/firecamp-platform/src/components/status-bar/StatusBarContainer.tsx
@@ -12,31 +12,44 @@ import { MetaBar } from './items/MetaBar';
 const firecampAgent: EFirecampAgent = _misc.firecampAgent();
 
 const StatusBarContainer: FC<any> = ({ className = '' }) => {
-  return (
-    <StatusBar className={className} id={'firecamp-status-bar'}>
-      <SwitchWorkspacePanel />
-      <StatusBar.PrimaryRegion id={'firecamp-status-bar-primary-region'}>
-        <div
-          tabIndex={1}
-          className="bg-primaryColor text-primaryColor-text w-fit px-3 flex items-center"
-          id={'status-bar-firecamp-version'}
-          data-tip={`Firecamp`}
-        >
-          <VscRemote size={12} />
-          <span className="pl-1">Firecamp</span>
-        </div>
-        <DDMenuContainer />
-      </StatusBar.PrimaryRegion>
-      <StatusBar.SecondaryRegion id={'firecamp-status-bar-secondary-region'}>
-        {firecampAgent === EFirecampAgent.Web && (
-          <div className="pr-3">
-            <FcAgentSelector />
-          </div>
-        )}
-        <MetaBar />
-      </StatusBar.SecondaryRegion>
-    </StatusBar>
-  );
+    return (
+        <StatusBar className={className} id={'firecamp-status-bar'}>
+            <SwitchWorkspacePanel />
+            <StatusBar.PrimaryRegion id={'firecamp-status-bar-primary-region'}>
+                <div
+                    tabIndex={1}
+                    className="bg-primaryColor text-primaryColor-text w-fit px-3 flex items-center"
+                    id={'status-bar-firecamp-version'}
+                    data-tip={`Firecamp`}
+                >
+                    <VscRemote size={12} />
+                    <span className="pl-1">Firecamp</span>
+                </div>
+
+
+                <div className="github-badge w-fit px-3 flex items-center">
+                    <a href="https://github.com/firecamp-dev/firecamp" target="_blank" rel="noopener noreferrer">
+                        <img src="https://img.shields.io/github/stars/firecamp-dev/firecamp?style=social" alt="GitHub Stars" />
+                    </a>
+                </div>
+                <div className="twitter-badge w-fit px-3 flex items-center">
+                    <a href="https://twitter.com/firecampdev" target="_blank" rel="noopener noreferrer">
+                        <img src="https://img.shields.io/twitter/follow/firecampdev?style=social" alt="Follow on Twitter" />
+                    </a>
+                </div>
+
+                <DDMenuContainer />
+            </StatusBar.PrimaryRegion>
+            <StatusBar.SecondaryRegion id={'firecamp-status-bar-secondary-region'}>
+                {firecampAgent === EFirecampAgent.Web && (
+                    <div className="pr-3">
+                        <FcAgentSelector />
+                    </div>
+                )}
+                <MetaBar />
+            </StatusBar.SecondaryRegion>
+        </StatusBar>
+    );
 };
 
 export default StatusBarContainer;
