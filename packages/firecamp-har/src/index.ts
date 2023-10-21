b09dae14e2d0af665810bea1f5a8a3ef4e05b5a3
jatin <jatingodnani@gmail.com>
2023-10-21 13:35:26 +0530

    makechanged-forchecking

--- packages/firecamp-har/src/index.ts
+++ packages/firecamp-har/src/index.ts
@@ -1,12 +1,72 @@
 import { Har } from 'har-format';
 import { Collection } from '@firecamp/types';
 
+
 const firecampToHar = (collection: Partial<Collection>): any => {
   return {};
 };
 
 const harToFirecamp = (har: Har): Partial<Collection> => {
-  return { requests: [] };
+  const Exctractquerparams=(queryparams:Har.queryString[])=>{
+    if(queryparams.length===0){
+      return []
+    }
+    const convertesqueryparams=queryparams.map((obj)=>{
+      return {
+        "id":"",
+        "key":obj.key,
+        "value":obj.value,
+        "type":"text",
+
+
+      }
+    })
+      return convertesqueryparams;
+  }
+  const Extractheader=(header:Har.headers[])=>{
+     if(header.length===0){
+      return []
+    }
+    return header.map((obj)=>{
+      return {
+        "name":obj.name,
+        "value":obj.value
+      }
+    })
+  }
+  return {
+    requests: [
+      {
+        "url": {
+          "raw": {har.entries.request.url},
+          "queryParams":Exctractquerparams(har.entries.request.queryString),
+          "pathParams": []
+        },
+        "method": {har.entries.request.methods},
+        "headers": Extractheader(har.entries.request.headers),
+        "body": {
+          "type": "none",
+          "value": ""
+        },
+        "auth": {
+          "type": "none",
+          "value": ""
+        },
+        "preScripts": [],
+        "postScripts": [],
+        "__meta": {
+          "name": "GET Request",
+          "description": "",
+          "type": "rest",
+          "version": "2.0.0"
+        },
+        "__ref": {
+          "id": ""
+        }
+      }
+
+    ]
+  };
 };
 
 export { firecampToHar, harToFirecamp };
