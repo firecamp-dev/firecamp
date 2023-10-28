aefa43435182652f7915808faf5cf0f799822179
Nishchit Dhanani <dnishchit@gmail.com>
2023-10-28 13:12:02 +0530

    chore: loadURL tried with local path

--- packages/firecamp-desktop/src/main.ts
+++ packages/firecamp-desktop/src/main.ts
@@ -6,7 +6,29 @@ const createWindow = () => {
     height: 600,
   });
 
-  win.loadURL('https://firecamp.dev');
+  if (app.isPackaged) {
+    // 'build/index.html'
+    // win.loadURL(`file://${__dirname}/../../build/prod/index.html`);
+  } else {
+    win.loadURL(`file://${__dirname}/../../../build/dev/index.html`);
+    // win.loadURL('http://localhost:3000');
+
+    win.webContents.openDevTools();
+
+    // Hot Reloading on 'node_modules/.bin/electronPath'
+    // require('electron-reload')(__dirname, {
+    //   electron: path.join(
+    //     __dirname,
+    //     '..',
+    //     '..',
+    //     'node_modules',
+    //     '.bin',
+    //     'electron' + (process.platform === 'win32' ? '.cmd' : '')
+    //   ),
+    //   forceHardReset: true,
+    //   hardResetMethod: 'exit',
+    // });
+  }
 };
 
 app.whenReady().then(() => {
