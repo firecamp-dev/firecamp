f28605337f9453a56c8a7fd024c40a4cf8c4e674
Nishchit Dhanani <dnishchit@gmail.com>
2023-11-07 16:49:39 +0530

    feat: preload script improved to handle invocation of multipart rest request for main ipc

--- packages/firecamp-electron/src/preload.ts
+++ packages/firecamp-electron/src/preload.ts
@@ -3,6 +3,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('__electron__', {
   http: {
     send: (request: any, variables: any) => {
+      if (request.body?.type == 'multipart/form-data') {
+        request.body = {
+          ...request.body,
+          value: interceptMultipartBody(request.body),
+        };
+      }
       return ipcRenderer.invoke('http:send', request, variables);
     },
     stop: (requestId: string) => ipcRenderer.invoke('http:stop', requestId),
@@ -13,3 +19,21 @@ contextBridge.exposeInMainWorld('__electron__', {
     },
   },
 });
+
+/* electron has limitation that we can not send DOM elements like files over ipc, thus we need to send only file meta including name, path and type */
+const interceptMultipartBody = (body: any) => {
+  const { value = [], type } = body;
+  return value.map((v: any) => {
+    if (v.file) {
+      return {
+        ...v,
+        file: {
+          name: v.file.name,
+          path: v.file.path,
+          type: v.file.type,
+        },
+      };
+    }
+    return v;
+  });
+};
