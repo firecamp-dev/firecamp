61dad6f860dfe25a3f480e5d5f2afc5e37a0e89f
Nishchit14 <dnishchit@gmail.com>
2024-01-02 15:18:41 +0530

    feat: mac app build and publish configuration has been setup #62

--- packages/firecamp-electron/scripts/notarize.js
+++ packages/firecamp-electron/scripts/notarize.js
@@ -1,17 +1,29 @@
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
+  console.log('notarisation started manually.');
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') {
+  if (
+    electronPlatformName !== 'darwin' ||
+    !process.env.AID ||
+    !process.env.APASS
+  ) {
+    console.log(
+      'not running notarize. platform is not macos or environment not set up.'
+    );
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  return notarize({
-    appBundleId: process.env.appBundleId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.AID,
-    appleIdPassword: process.env.APASS,
-  });
+  try {
+    return await notarize({
+      appBundleId: process.env.appBundleId,
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.AID,
+      appleIdPassword: process.env.APASS,
+    });
+  } catch (e) {
+    console.log('notarize error', e);
+  }
 };
