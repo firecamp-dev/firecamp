59a8dc879c1133e77a15e5f76ef168f4a382d3eb
Nishchit14 <dnishchit@gmail.com>
2024-03-04 16:52:03 +0530

    feat: relainch fn implemented

--- packages/firecamp-electron/src/updater/dialogs.ts
+++ packages/firecamp-electron/src/updater/dialogs.ts
@@ -2,17 +2,21 @@ import { app, dialog } from 'electron';
 import { UpdateInfo } from 'electron-updater';
 
 const showUpdateDownloadedWinDialog = (win: any) => {
-  dialog.showMessageBoxSync(win, {
+  const index = dialog.showMessageBoxSync(win, {
     type: 'question',
     title: '👏️ Installed New Version',
     message: 'The new version is installed. You may now restart the app.',
     detail: 'Please ensure you save any open, unsaved work before proceeding.',
     buttons: ['Cancel', 'Restart'],
   });
+  if (index == 1) {
+    app.relaunch();
+    app.exit();
+  }
 };
 
 const showUpdateDownloadedMacDialog = (win: any) => {
-  dialog.showMessageBoxSync(win, {
+  const index = dialog.showMessageBoxSync(win, {
     type: 'question',
     title: '👏️ Installed New Version',
     message: 'The new version is installed. You may now restart the app.',
@@ -20,24 +24,36 @@ const showUpdateDownloadedMacDialog = (win: any) => {
     buttons: ['Cancel', 'Relaunch'],
     noLink: true,
   });
+  if (index == 1) {
+    app.relaunch();
+    app.exit();
+  }
 };
 
 const showUpdateAvailable = (win: any) => {
-  dialog.showMessageBoxSync(win, {
+  const index = dialog.showMessageBoxSync(win, {
     type: 'info',
     title: 'Update Found',
     message: 'Found updates, do you want to update now?',
     buttons: ['Sure', 'No'],
   });
+  if (index == 1) {
+    app.relaunch();
+    app.exit();
+  }
 };
 
 const showNoUpdateAvailable = (win: any, info: UpdateInfo) => {
-  dialog.showMessageBoxSync(win, {
+  const index = dialog.showMessageBoxSync(win, {
     type: 'info',
     title: 'No Updates Available',
     message: `No updates available. \n\n You're using the latest version Firecamp v${info.version}.`,
     buttons: ['Ok'],
   });
+  if (index == 1) {
+    app.relaunch();
+    app.exit();
+  }
 };
 
 export {
